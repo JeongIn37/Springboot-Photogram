891b4f0e4cd2d2ca5fc6ab37315c82843cad8129
yoonjeongin <yoonjeongin@naver.com>
2022-07-05 18:21:41 +0900

    [Add] JPA ORM: Create User Model

---
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -0,0 +1,52 @@
+package com.cos.photogramstart.domain.user;
+
+// JPA - Java Persistence API (자바로 데이터를 영구적으로 저장할 수 있는 API를 제공)
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor // 전체 생성자
+@NoArgsConstructor  // 빈 생성자
+@Data   // Getter & Setter
+@Entity // DB에 테이블 생성
+public class User {
+
+    @Id // pk 생성
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 DB를 따라감 (mysql-AutoIncrement, oracle-sequence )
+    private int id;
+
+    private String username;
+    private String password;
+
+    private String name;
+    private String website; // 웹 사이트
+    private String bio; // 자기소개
+    private String email;
+    private String phone;
+    private String gender;
+
+    private String profileImageUrl; // 사진
+    private String role;    // 권한
+
+    private LocalDateTime createDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    // create date 자동 생성
+    @PrePersist // DB에 insert 되기 직전에 실행
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+}
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,11 +1,16 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller //1.IoC에 등록됨 2.file을 리턴하는 컨트롤러
 public class AuthController {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
     @GetMapping("/auth/signin")
     public String signinForm(){
@@ -20,7 +25,8 @@ public class AuthController {
     // 회원가입 버튼 -> /auth/signup -> /auth/siginin
     // csrf 토큰이 활성화 되어 있어서 실행이 안 됨
     @PostMapping("/auth/signup")    // 회원가입
-    public String signup(){
+    public String signup(SignupDto signupDto){ //  key=value 형태(x-www-form-urlencoded)
+        log.info(signupDto.toString());
         return "auth/signin";   // 회원가입 성공시 로그인 페이지로
     }
 }
