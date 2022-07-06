2967a384b89ab1331fee5453744239d941341dc6
yoonjeongin <yoonjeongin@naver.com>
2022-07-06 18:25:12 +0900

    [Add] Signup Exception using Script & CMRespDto

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.cos.photogramstart.handler;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
+import com.cos.photogramstart.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,14 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)   // Runtime exception이 발생하는 모든 exception을 가로챔
-    public Map<String, String> validationException(CustomValidationException e){
-        return e.getErrorMap();
+    public String validationException(CustomValidationException e){   // ?: 리턴시에 자동으로 바
+
+        /* CMRespDto, Script 비교
+            1. 클라이언트에게 응답할 때: Script가 좋음
+            2. Ajax 통신 - CMRespDto
+            3. Android 통신 - CMRespDto
+         */
+        return Script.back(e.getErrorMap().toString());
+        //return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
     }
 }
