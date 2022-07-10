7ae196e8b41d04691cd6eb5253876918a377cb19
yoonjeongin <yoonjeongin@naver.com>
2022-07-10 17:28:28 +0900

    [Add] non-exist User id error popup - User Certification Finished!

--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -2,17 +2,22 @@ package com.cos.photogramstart.web.api;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
 import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.service.UserService;
 import com.cos.photogramstart.web.dto.CMRespDto;
 import com.cos.photogramstart.web.user.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,12 +29,20 @@ public class UserApiController {
     public CMRespDto<?> update(
             @PathVariable int id,
             @Valid UserUpdateDto userUpdateDto,
-            BindingResult bindingResult, // 꼭 @Valid 가 적혀있는 다음 파라메터 적어야됨
+            BindingResult bindingResult, // 꼭 @Valid 가 적혀있는 다음 파라메터 적어야됨 (위치 중요!!)
             @AuthenticationPrincipal PrincipalDetails principalDetails) {
 
-        User userEntity = userService.회원수정(1, userUpdateDto.toEntity());
-        principalDetails.setUser(userEntity); // 세션 정보 변경
-        return new CMRespDto<>(1, "회원수정완료", userEntity); // 응답시에 userEntity의 모든 getter 함수가 호출되고 JSON으로 파싱하여 응답한다.
-
+        // 에러 메시지 처리
+        if(bindingResult.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error: bindingResult.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new CustomValidationApiException("유효성 검사 실패", errorMap);
+        }else {
+            User userEntity = userService.회원수정(1, userUpdateDto.toEntity());
+            principalDetails.setUser(userEntity); // 세션 정보 변경
+            return new CMRespDto<>(1, "회원수정완료", userEntity); // 응답시에 userEntity의 모든 getter 함수가 호출되고 JSON으로 파싱하여 응답한다.
+        }
     }
 }
