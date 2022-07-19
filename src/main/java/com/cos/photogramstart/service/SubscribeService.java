822402def1f164609a2aa6b5ee0d29561b5d9ae2
yoonjeongin <yoonjeongin@naver.com>
2022-07-19 23:16:44 +0900

    [Chore] Move Subscribe to domain folder

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -1,14 +1,21 @@
 package com.cos.photogramstart.service;
 
-import com.cos.photogramstart.domain.user.subscribe.Subscribe;
+import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class SubscribeService {
 
+    private final SubscribeRepository subscribeRepository;
+
+
     @Transactional  // insert, delete 등 DB에 영향을 줌
     public void 구독하기(int fromUserId, int toUserId){
+
+        subscribeRepository.save(null);
 
     }
 
